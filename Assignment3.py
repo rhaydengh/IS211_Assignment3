7269747a4cb6405cc93fd326c907423f1ea53420
rhaydengh <hayden.becca@gmail.com>
2017-09-24 15:14:35 -0400

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assignment 3"""
+
+import argparse
+import urllib2
+import csv
+from StringIO import StringIO
+import re
+
+response = urllib2.urlopen('http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv')
+return response.read()
+
+
+def downloadData(url):
+    """This function downloads data from a url"""
+
+    data = urllib2.urlopen(url)
+    csvdata = data.read()
+
+def parse_csv(csvdata):
+    """
+    Process the csv data
+    :param csvdata:
+    :return:percentage of image requests, most popular browser
+    """
+    count_img = 0
+    count_rows = 0
+    reader = csv.reader(StringIO(csvdata))
+
+    count_msie = 0
+    count_safari = 0
+    count_firefox = 0
+
+    for row in reader:
+        count_rows += 1
+        if re.search("jpg|png|gif", row[0], re.I):
+            count_img += 1
+        if re.search("MSIE", row[2]):
+            count_msie += 1
+        if re.search("Safari", row[2]):
+            count_safari += 1
+        if re.search("Firefox", row[2]):
+            count_firefox += 1
+    print "The percentage of image requests is",(100 * count_img/count_rows),"%"
+
+    if max(count_firefox, count_msie, count_safari) == count_safari:
+        print "The most popular browser today is Safari"
+    elif max(count_firefox, count_msie, count_safari) == count_msie:
+        print "Internet Explorer"
+    elif max(count_firefox, count_msie, count_safari) == count_firefox:
+        print "Firefox"
